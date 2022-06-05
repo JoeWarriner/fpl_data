2dca290f3a9e90b020f858b83aee37ab39447cc0
Joe Warriner <joewarriner40@gmail.com>
2022-06-05 15:24:00 +0100

    Removed injury status from tables, added short_name for teams,  migrated

--- predictor/models.py
+++ predictor/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Team(models.Model):
-    team_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=5)
     this_season_id = models.IntegerField()
 
     def __str__(self) -> str:
@@ -17,14 +18,12 @@ class Player(models.Model):
     expected_points = models.IntegerField()
     position = models.CharField(max_length=50)
     current_team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    injury_states = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.first_name} {self.second_name}'
 
 class PlayerFixture(models.Model):
     points_scored = models.IntegerField()
-    now_injury_status = models.IntegerField()
     season = models.IntegerField()
     gameweek = models.IntegerField()
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
