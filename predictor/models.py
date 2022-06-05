02522d92e7684f47f705059618cece4e9737df3a
Joe Warriner <joewarriner40@gmail.com>
2022-06-05 14:13:21 +0100

    Added initial database functionality

--- predictor/models.py
+++ predictor/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=200)
+    this_season_id = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.team_name
+
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=200)
+    second_name = models.CharField(max_length=200)
+    this_season_id = models.IntegerField()
+    current_value = models.IntegerField()
+    expected_points = models.IntegerField()
+    position = models.CharField(max_length=50)
+    current_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    injury_states = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
+
+class PlayerFixture(models.Model):
+    points_scored = models.IntegerField()
+    now_injury_status = models.IntegerField()
+    season = models.IntegerField()
+    gameweek = models.IntegerField()
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    team_for = models.ForeignKey(Team, related_name='team_for', on_delete=models.CASCADE)
+    team_against = models.ForeignKey(Team, related_name='team_against', on_delete=models.CASCADE)
+    position = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.player}, week: {self.gameweek}, {self.season}'
+
+
+
