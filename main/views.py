6f2764aab89e03c61a12f97a5ffe28955a97dc99
Joe Warriner <joewarriner40@gmail.com>
2023-02-16 19:39:29 +0000

    Added some nicer styling to the front page

--- main/views.py
+++ main/views.py
@@ -7,7 +7,5 @@ import os
 class Home(View):
 
     def get(self, request):
-        with open(Path(os.getcwd(), 'README.md')) as file:
-            context = {'markdown_lines': file.readlines() }
-            return render(request, 'main/home.html', context)
+        return render(request, 'main/home.html')
 
