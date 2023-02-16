935920e191ca2db724bbfb9ca2cf4e70a448b4e7
Joe Warriner <joewarriner40@gmail.com>
2023-02-16 18:18:42 +0000

    Renamed main directory

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from fpl.views import Home
+from main.views import Home
 from django.urls import path , include
 
 urlpatterns = [
